b571df0671d4b5a1214a4d3aa11b4a800ab26a39
Kina Lee <kina.lee@holbertonschool.com>
2017-05-05 22:23:04 +0000

    added newlines

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+
   let checkedList = []; // List to save checked elements
+
   $('input[type="checkbox"]').change(function () {
+
     if (this.checked) {
       checkedList.push($(this).attr('data-id'));
     } else {
@@ -8,10 +11,13 @@ $(document).ready(function () {
     }
 
     let aList = $('div.amenities h4');
+
     if (checkedList.length === 0) {
       aList.text('\u00A0'); // fill in the h4 tag with blank
     } else {
       $('div.amenities h4').text(checkedList.join(', '));
     }
+
   });
+
 });
