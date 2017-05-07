fdd6e9c23b10a5fb61300aaa60f44b884ee5a657
Kina Lee <kina.lee@holbertonschool.com>
2017-05-07 09:11:07 +0000

    100 wip

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-  // Listens for changes on each INPUT checkbox tag
-  let checkedObj = {}; // Object to save checked elements
+  // Listens for changes on each amenity input checkbox tag
+  let amenityObj = {}; // Object to save checked amenity elements
 
-  $('input[type="checkbox"]').change(function () {
+  $('div.amenities input[type="checkbox"]').change(function () {
     if (this.checked) {
-      checkedObj[$(this).attr('data-name')] = $(this).attr('data-id');
+      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
     } else {
-      delete checkedObj[$(this).attr('data-name')];
+      delete amenityObj[$(this).attr('data-name')];
     }
 
-    if (Object.keys(checkedObj).length < 1) {
+    if (Object.keys(amenityObj).length < 1) {
       $('div.amenities h4').text('\u00A0'); // fill in the h4 tag with blank
       // optional
       // $('div.amenities h4').html('&nbsp;');
     } else {
-      $('div.amenities h4').text(Object.keys(checkedObj).join(', '));
+      $('div.amenities h4').text(Object.keys(amenityObj).join(', '));
       // optional
-      // $('div.amenities h4').text($.map(checkedObj, function (name) { return name; }).join(', '));
+      // $('div.amenities h4').text($.map(amenityObj, function (name) { return name; }).join(', '));
     }
   });
 
@@ -44,6 +44,7 @@ $(document).ready(function () {
     if (obj === undefined) {
       obj = '{}';
     }
+    console.log(obj);
     $('section.places').empty();
     $('section.places').append($('<h1>').text('Places'));
     $.ajax({
@@ -72,11 +73,47 @@ $(document).ready(function () {
   // Filters places when search button is clicked
   $('section.filters button').click(function () {
     let obj = {};
-    if (checkedObj.length < 1) {
-      obj = '{}';
-    } else {
-      obj = JSON.stringify({'amenities': Object.values(checkedObj)});
+
+    if (Object.keys(amenityObj).length > 0) {
+      console.log(amenityObj);
+      obj["amenities"] = Object.values(amenityObj);
     }
-    placeGenerator(obj);
+    if (stateArray.length > 0) {
+      obj["states"] = stateArray;
+    }
+    if (cityArray.length > 0) {
+      obj["cities"] = cityArray;
+    }
+    placeGenerator(JSON.stringify(obj));
+  });
+
+  // --------------------------------------------------------------------
+
+  // Listens for changes on each state and city input checkbox tag
+  let locationObj = {};
+  let stateArray = [];
+  let cityArray = [];
+
+  $('div.locations input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      locationObj[$(this).attr('data-name')] = $(this).attr('data-id');
+      if ($(this).attr('class') === 'state') {
+        stateArray.push($(this).attr('data-id'));
+      } else {
+        cityArray.push($(this).attr('data-id'));
+      }
+    } else {
+      delete locationObj[$(this).attr('data-name')];
+      if ($(this).attr('class') === 'state') {
+        stateArray.pop($(this).attr('data-id'));
+      } else {
+        cityArray.pop($(this).attr('data-id'));
+      }
+    }
+    if(Object.keys(locationObj).length < 1) {
+      $('div.locations h4').text('\u00A0');
+    } else {
+      $('div.locations h4').text(Object.keys(locationObj).join(', '));
+    }
   });
 });
